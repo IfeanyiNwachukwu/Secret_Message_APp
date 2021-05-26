60b5ef8cdda046901a3d024683c74762f90c8f14
Ifeanyi Nwachukwu <ifenwachukwu7@gmail.com>
2021-05-26 21:15:54 +0100

    Generating the Url

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault(); // Allows us time to get form details
    const input = document.querySelector('#message-input');
    const encrypted = btoa(input.value);
-   console.log(input.value);
-
-   linkInput =  document.querySelector('#link-input');
-   linkInput.value = '';
-   linkInput.value = encrypted;
-
-//    document.querySelector('#link-input').value = encrypted;
+   
+   const linkInput = document.querySelector('#link-input');
+   linkInput.value = `${window.location}#${encrypted}`;
+   linkInput.select();
 });
