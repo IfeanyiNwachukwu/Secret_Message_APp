a0daf59ab4191047c9c6185d81eb60098b1cce71
Ifeanyi Nwachukwu <ifenwachukwu7@gmail.com>
2021-05-26 22:59:42 +0100

    displaying the message

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+const {hash} = window.location;
+    const message = atob(hash.replace('#',''));
+    if(message){
+        document.querySelector('#message-form').classList.add('hide');
+        document.querySelector('#message-show').classList.remove('hide');
+        document.querySelector('h1').innerHTML = message;
+    }
+
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault(); // Allows us time to get form details
-
-    document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#message-form').classList.add('hide');
     document.querySelector('#link-form').classList.remove('hide');
 
    const input = document.querySelector('#message-input');
